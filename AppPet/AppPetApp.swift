aa616a9dc8182ce9c2d9fd97b98b840e507b236a
Acuiza <Acuiza@hypernovalabs.com>
2024-02-13 18:31:13 -0400

    Initial Commit

---
+++ AppPet/AppPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPetApp.swift
+//  AppPet
+//
+//  Created by Alvaro Cuiza on 13/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AppPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
